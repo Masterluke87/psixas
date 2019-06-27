435980abade2deb30643c80be895b7f0003e6323
Masterluke <master.luke87@gmail.com>
2019-06-27 18:48:56 +0200

    Better spec.py output

--- spec.py
+++ spec.py
@@ -5,11 +5,11 @@ import pickle
 
 
 def CalcSpec(mol,func):
-    psi4.core.print_out("\n\nX-Ray Absorption Spectrum Calcaultion:\n"+39*"="+"\n\n")
+    psi4.core.print_out("\n\nX-Ray Absorption Spectrum Calculation:\n"+38*"="+"\n\n")
     prefix = psi4.core.get_local_option("PSIXAS","PREFIX")
 
 
-     psi4.core.print_out("Using orbitals, occupations from file: {}  \n".format(prefix+"_exorbs.npz"))
+    psi4.core.print_out("Using orbitals, occupations from file: {}  \n".format(prefix+"_exorbs.npz"))
 
     Ca = np.load(prefix+"_exorbs.npz")["Ca"]
     Cb = np.load(prefix+"_exorbs.npz")["Cb"]
@@ -23,12 +23,15 @@ def CalcSpec(mol,func):
     orbitals = np.load(prefix+"_exorbs.npz", allow_pickle=True)["orbitals"]
 
 
-    psi4.core.print_out("Occupation pattern: ")
-    psi4.core.print_out("{} \n".format("".join(occa))
-    psi4.core.print_out("{} \n".format("".join(occb))
-    
+    psi4.core.print_out("Occupation pattern: \n")
+
+    printOccupation("Alpha",occa,15)
+    printOccupation("Beta ",occb,15)
+
+    psi4.core.be_quiet()
     wfn   = psi4.core.Wavefunction.build(mol,psi4.core.get_global_option('BASIS'))
     mints = psi4.core.MintsHelper(wfn.basisset())
+    psi4.core.reopen_outfile()
 
     D = mints.ao_dipole()
     Dx,Dy,Dz = np.asarray(D[0]),np.asarray(D[1]),np.asarray(D[2])
@@ -36,12 +39,12 @@ def CalcSpec(mol,func):
     spec = {}
 
     if ("b" in [x['spin'] for x in orbitals]):
-        psi4.core.print_out("BETA orbitals")
+        psi4.core.print_out("\nBETA orbitals")
         orbI = ([c for c,x in enumerate(occb) if x != 1.0][0])
-        psi4.core.print_out("Initial Orbital: {:d}".format(orbI))
+        psi4.core.print_out("\nInitial Orbital: {:d}".format(orbI))
         Ci = Cb[orbI]
         orbF = ([c for c,x in enumerate(occb) if (x != 1.0) and (c!=orbI)])
-        psi4.core.print_out("Final Orbitals: {} "+str("".join(orbF))
+        psi4.core.print_out("\nFinal Orbitals: {} \n\n".format(str(orbF)))
         Cf = Cb[orbF]
 
         Mx = Cb.T @ Dx @ Cb
@@ -55,7 +58,20 @@ def CalcSpec(mol,func):
 
         with open(prefix+'_b.spectrum', 'wb') as handle:
             pickle.dump(spec, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
+        psi4.core.print_out(("\n{}"+"_b.spectrum written.. \n\n").format(prefix))
 
     if ("a" in [x['spin'] for x in orbitals]):
         print ("ALFA orbitals")
+
+def printOccupation(title,occs,width):
+    psi4.core.print_out("\n{}: \n".format(title))
+    for i in range(int(len(occs)/width)):
+        psi4.core.print_out(("|"+"{:^4}|"*width).format(*[x for x in range(i*width,i*width+ width)]))
+        psi4.core.print_out(("\n|"+"{:3.2f}|"*width+"\n\n").format(*[x for x in occs[i*width:i*width+width]]))
+    
+    psi4.core.print_out(("|"+"{:^4}|"*(len(occs) % width)).format(*[x for x in range(int(len(occs)/width)*width,len(occs))]))
+    psi4.core.print_out(("\n|"+"{:3.2f}|"*(len(occs) % width)+"\n\n").format(*[x for x in occs[int(len(occs)/width)*width:]]))
+
+ 
+
+
